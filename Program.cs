149f2778631191b82bc2902c3a67aadeb37de44f
Alexey Krymov <krymovalexey@gmail.com>
2022-09-07 03:12:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(Sqr(2));
+
+            int Sqr(int x)
+            {
+                int result = x * x;
+                return result;
+            }
+
+        }
+
+    }
+}
